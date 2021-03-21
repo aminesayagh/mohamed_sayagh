f1cc55b1a32733af90ff633523947658b5304028
aminesayagh <aminesayagh1997@gmail.com>
2021-03-21 12:32:13 +0100

    import

--- components/uiElement/popupInfo.vue
+++ components/uiElement/popupInfo.vue
@@ -7,7 +7,7 @@
 <style lang="less" scoped>
 @layer components {
       .popContent{
-            @apply fixed top-0 right-0 left-0 bg-black_11 op;
+            @apply fixed top-0 right-0 left-0 bg-black_11;
       }
 }
 
