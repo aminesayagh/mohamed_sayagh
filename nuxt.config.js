46b2bbe9689c8cf2a84350e66e15644592f4879f
aminesayagh <aminesayagh1997@gmail.com>
2021-03-21 12:04:07 +0100

    import

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,7 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/png', href: '/mohamed_logo.png' }
     ]
   },
 
