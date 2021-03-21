4bf926da1f5ed937fc86b87772aed4d3373117db
aminesayagh <aminesayagh1997@gmail.com>
2021-03-21 12:07:30 +0100

    import

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,6 +10,7 @@ export default {
   router: { base: '/mohamed_sayagh/' },
   head: {
     titre: 'mohamed_sayagh',
+    titleTemplate: 'Dr.MOHAMMED SAYAGH',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
